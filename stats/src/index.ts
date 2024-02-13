85e4f957e6782cb695440540d87adba2e197e98d
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-02-14 01:00:39 +0300

    ADD study on enums

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,30 @@
-console.log('hello world');
+import fs from 'fs';
+
+const matches = fs
+	.readFileSync('football.csv', {
+		encoding: 'utf-8',
+	})
+	.split('\n')
+	.map((line): string[] => {
+		return line.split(',');
+	});
+
+// console.log(matches);
+
+let manUnitedWins = 0;
+
+enum MatchResult {
+	HomeWin = 'H',
+	AwayWin = 'A',
+	Draw = 'D',
+}
+
+for (let match of matches) {
+	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+		manUnitedWins++;
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+		manUnitedWins++;
+	}
+}
+
+console.log(manUnitedWins);
